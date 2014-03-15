1352c21ae1d28ba10dd43dd2d3fa1488a4f21a96
repositoriy <sergeychemisov@mail.ru>
2014-03-15 17:27:24 +0300

    Refactor ResorceFactoryTests

--- src/main/java/resourceSystem/ResourceFactoryTests.java
+++ src/main/java/resourceSystem/ResourceFactoryTests.java
@@ -2,6 +2,7 @@ package resourceSystem;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.DOMException;
 import org.xml.sax.SAXException;
@@ -12,19 +13,40 @@ import java.io.IOException;
 
 public class ResourceFactoryTests {
 
+    ResourceFactory resourceFactory;
+
+    @Before
+    public void testBefore() throws ParserConfigurationException, SAXException, IOException, InstantiationException, IllegalAccessException, ClassNotFoundException, DOMException, NoSuchFieldException, SecurityException, IllegalArgumentException {
+        resourceFactory = ResourceFactory.getInstance();
+    }
+
     @Test
-    public void testNewInstance() throws ParserConfigurationException, SAXException, IOException, InstantiationException, IllegalAccessException, ClassNotFoundException, DOMException, NoSuchFieldException, SecurityException, IllegalArgumentException {
-        ResourceFactory resourceFactory = ResourceFactory.getInstance();
+    public void testNewInstance() {
         Assert.assertEquals(resourceFactory, ResourceFactory.getResourceFactory());
+    }
+
+    @Test
+    public void testVfsInNewInstance() {
         Assert.assertEquals(resourceFactory.getVfs(), VirtualFileSystemImpl.getVirtualFileSystemImpl());
+    }
+
+    @Test
+    public void testGetResourseInNewInstance() {
         Assert.assertNotNull(resourceFactory.getResources());
+    }
+
+    @Test
+    public void testFileNotExist() {
         Assert.assertNull(resourceFactory.get("myFile.xml"));
+    }
+
+    @Test
+    public void testFileExist() {
         Assert.assertNotNull(resourceFactory.get("GameResource.xml"));
     }
 
     @Test
     public void testGetResourceFromFile() throws ParserConfigurationException, SAXException, IOException, InstantiationException, IllegalAccessException, ClassNotFoundException, DOMException, NoSuchFieldException, SecurityException, IllegalArgumentException {
-        ResourceFactory resourceFactory = ResourceFactory.getInstance();
         File file = new File("data//GameResource.xml");
         Assert.assertNotNull(resourceFactory.getResourcesFromFile(file));
     }
