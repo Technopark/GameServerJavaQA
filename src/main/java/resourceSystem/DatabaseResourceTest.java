b1f219c6f253d1d456ed7da819c93e44a9009c48
repositoriy <sergeychemisov@mail.ru>
2014-03-15 16:24:14 +0300

    DatabaseResource Test

---
+++ src/main/java/resourceSystem/DatabaseResourceTest.java
@@ -0,0 +1,20 @@
+package resourceSystem;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+public class DatabaseResourceTest {
+
+    @Test
+    public void testDatabaseResourse() {
+        DatabaseResource dbResourse = new DatabaseResource();
+        assertNull(dbResourse.getDialect());
+        assertNull(dbResourse.getDriver_class());
+        assertNull(dbResourse.getPassword());
+        assertNull(dbResourse.getShow_sql());
+        assertNull(dbResourse.getStatement());
+        assertNull(dbResourse.getUrl());
+        assertNull(dbResourse.getUsername());
+    }
+}
