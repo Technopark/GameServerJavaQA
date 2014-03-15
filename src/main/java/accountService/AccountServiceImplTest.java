314d1bfe95bedec321a74b5c99e6ac620a495216
repositoriy <sergeychemisov@mail.ru>
2014-03-15 14:10:47 +0300

    AccountService Tests with Before Test

--- src/main/java/accountService/AccountServiceImplTest.java
+++ src/main/java/accountService/AccountServiceImplTest.java
@@ -3,14 +3,22 @@ package accountService;
 import base.MessageSystem;
 import messageSystem.MessageSystemImpl;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AccountServiceImplTest {
 
+    MessageSystem ms;
+    AccountServiceImpl accountService;
+
+    @Before
+    public void testBefore() {
+        ms = new MessageSystemImpl();
+        accountService = new AccountServiceImpl(ms, null);
+    }
+
     @Test
     public void testAccountServiceImpl() {
-        MessageSystem ms = new MessageSystemImpl();
-        AccountServiceImpl accountService = new AccountServiceImpl(ms, null);
         Assert.assertEquals(accountService.getMessageSystem(), ms);
         Assert.assertNull(accountService.getResource());
         Assert.assertNotNull(accountService.getAddress());
@@ -18,8 +26,6 @@ public class AccountServiceImplTest {
 
     @Test
     public void testInitGamer() {
-        MessageSystem ms = new MessageSystemImpl();
-        AccountServiceImpl accountService = new AccountServiceImpl(ms, null);
         accountService.initGamer(null, 100);
         Assert.assertEquals(accountService.getUserId().get(null).intValue(), accountService.getUserIdGenerator().intValue());
     }
